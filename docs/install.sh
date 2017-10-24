5483a7ff5a5646a71f74e5821f2b273388f31d88
kk.kim <kk.kim@pitple.com>
2017-10-25 00:56:59 +0900

    supervisord

--- docs/install.sh
+++ docs/install.sh
@@ -3,39 +3,41 @@
 # OWNER="kk"
 # SERVICE="ncloud"
 # POOL="asia.cryptonight-hub.miningpoolhub.com:20580"
-# RUN_TYPE="none" / shell / screen
+
+# Install XMR
+echo "[`date`] == Install XMR =="
 
 MINER=$1
 OWNER=$2
 SERVICE=$3
 POOL=$4
-RUN_TYPE=$5
 WORKER="$MINER.$OWNER-$SERVICE-$HOSTNAME"
+XMR_DIR=/usr/local/xmr-stak-cpu
 
-# Install xmr
-echo "[`date`] == Install xmr =="
+# apt update & install
+echo "[`date`] == apt update & install =="
 sudo apt-get update
-sudo apt-get install -y git libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt-get install -y git libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev supervisor
 
 # Clone sources if not exist
-echo "[`date`] == Clone Sources =="
-if [ ! -d ~/xmr-stak-cpu ]; then 
-    cd ~
-    sudo git clone https://github.com/kkkim-pitple/xmr-stak-cpu.git
-    cd ~/xmr-stak-cpu
+if [ ! -d $XMR_DIR ]; then
+    echo "[`date`] == Clone Sources =="
+    sudo git clone https://github.com/kkkim-pitple/xmr-stak-cpu.git $XMR_DIR
+    cd $XMR_DIR
     sudo cmake .
     sudo make install
+else
+    echo "[`date`] == SKIP : Clone Sources =="
 fi
 
-# Update Config.txt
-echo "[`date`] == Update Config.txt =="
+# XMR Config.txt
+echo "[`date`] == XMR Config.txt =="
 #echo `lscpu | grep -v -i flags:`
 SOCKETS=`lscpu | grep -i '^Socket(s):' | head -1 | awk '{ print $2 }'`
 CORES_PER_SOCKET=`lscpu | grep -i '^Core(s) per socket:' | head -1 | awk '{ print $4 }'`
 THREADS_PER_CORE=`lscpu | grep -i '^Thread(s) per core:' | head -1 | awk '{ print $4 }'`
 CPUS=`lscpu | grep -i '^CPU(s):' | head -1 | awk '{ print $2 }'`
 #echo $SOCKETS, $CORES_PER_SOCKET, $THREADS_PER_CORE, $CPUS
-
 CPU_THREADS_CONF="[\n"
 for (( i=0; i<$CPUS/$THREADS_PER_CORE; i++ ))
 do
@@ -48,39 +50,51 @@ sudo sed -i -r \
     -e 's/^("pool_address" : ).*,/\1"'$POOL'",/' \
     -e 's/^("wallet_address" : ).*,/\1"'$WORKER'",/' \
     -e 's/^("pool_password" : ).*,/\1"x",/' \
-    ~/xmr-stak-cpu/bin/config.txt
+    "$XMR_DIR/bin/config.txt"
 
 # Update System Memory Config
-echo "[`date`] == Update System Memory Config =="
+echo "[`date`] == /etc/security/limits.conf =="
 LIMITS_CONF=/etc/security/limits.conf
 grep -q -P "\*\tsoft\tmemlock\t262144" $LIMITS_CONF || sudo echo -e "*\tsoft\tmemlock\t262144" >> $LIMITS_CONF
 grep -q -P "\*\thard\tmemlock\t262144" $LIMITS_CONF || sudo echo -e "*\thard\tmemlock\t262144" >> $LIMITS_CONF
+XMR_REBOOT_CHECK="# XMR REBOOT CHECK - DO NOT REMOVE"
+XMR_REBOOT=`grep -c $XMR_REBOOT_CHECK $LIMITS_CONF`
+XMR_REBOOT || sudo echo -e $XMR_REBOOT_CHECK >> $LIMITS_CONF
 #cat $LIMITS_CONF
 
 # Update Update Enable HugePage
-echo "[`date`] == Update Enable HugePage =="
+echo "[`date`] == HugePage =="
 HUGE_PAGE="vm.nr_hugepages=128"
 HUGE_PAGE_CONF=/etc/sysctl.conf
 grep -q $HUGE_PAGE $HUGE_PAGE_CONF || sudo echo -e "\n# HugePages\n$HUGE_PAGE" >> $HUGE_PAGE_CONF
 #cat $HUGE_PAGE_CONF
-
 sudo sysctl -w $HUGE_PAGE
 sudo sysctl -p
+
+# supervisor setting
+#sudo systemctl start supervisor
+#sudo systemctl enable supervisor 
+
+SUPERVISOR_CONF=/etc/supervisor/conf.d/xmr-stak-cpu.conf
+if [ ! -f $SUPERVISOR_CONF ]; then
+    echo "[`date`] == supervisor setting =="
+    sudo echo "[program:xmr-stak-cpu]" >> $SUPERVISOR_CONF
+    sudo echo "command = $XMR_DIR/bin/xmr-stak-cpu" >> $SUPERVISOR_CONF
+    sudo echo "directory = $XMR_DIR/bin" >> $SUPERVISOR_CONF
+    sudo echo "autostart=true" >> $SUPERVISOR_CONF
+    sudo echo "autorestart=true" >> $SUPERVISOR_CONF
+    sudo echo "startretries=10" >> $SUPERVISOR_CONF
+    sudo echo "redirect_stderr=true" >> $SUPERVISOR_CONF
+    sudo supervisor update
+else
+    echo "[`date`] == SKIP : supervisor setting =="
+fi
 
 # Done!
 echo "[`date`] == Install Done! =="
 
-
-# Run Mining
-if [ $RUN_TYPE = "shell" ]
-then
-    echo "[`date`] == Run Mining : shell =="
-    cd ~/xmr-stak-cpu/bin
-    sudo ./xmr-stak-cpu
-elif [ $RUN_TYPE = "screen" ]
-then
-    echo "[`date`] == Run Mining : screen =="
-    cd ~/xmr-stak-cpu/bin
-    screen sudo ./xmr-stak-cpu
+# Reboot if needed
+if [ $XMR_REBOOT -eq 0 ]; then
+    echo "[`date`] == reboot =="
+#    sudo reboot
 fi
-
